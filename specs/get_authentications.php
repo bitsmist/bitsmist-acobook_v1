8f39f317b9394e48258d619fb1fe4759f5688f22
Masaki Yasutake <my17560@gmail.com>
2019-10-02 21:30:13 +0900

    Added fields specification.

--- specs/get_authentications.php
+++ specs/get_authentications.php
@@ -14,25 +14,25 @@ return [
 		"password" 			=> [ "fieldType" => "TEXT" ],
 	],
 
+	// Fields
+
+	"fields" => [
+		"id"			=> [],
+		"user_nm"		=> [],
+	],
+
 	// Search conditions
 
 	"searches" => [
 		"default" => [
 			[ "type" => "parameters", "operator" => "AND", "fields" => [
 					[ "type"=>"parameter", "parameter" => "user", "field" => "user_nm", "comparer" => "=", "compareType" => "item" ],
+					[ "type"=>"parameter", "parameter" => "password", "field" => "password_txt", "comparer" => "=", "compareType" => "item" ],
 				]
 			],
 			[ "field" 		=> "delete_dt", "compare" => "=", "value" => "@NULL@" ],
 			[ "type" => "operator", "value" => "AND" ],
 		]
 	],
-
-	/*
-	"options" => [
-		"needPreflight" => false,
-		"passwordParameterName" => "password",
-		"passwordFieldName" => "password",
-	],
-	 */
 
 ];
