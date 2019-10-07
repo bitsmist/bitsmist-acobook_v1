7d7e34a515da9d63bbb88c76182f9d23542f1605
Masaki Yasutake <my17560@gmail.com>
2019-10-07 20:21:25 +0900

    working...

---
+++ specs/delete_transactions.php
@@ -0,0 +1,16 @@
+<?php
+return [
+
+	// Databases
+
+	"databases"	=> [
+		"sqlite"			=> [ "tableName" => "transaction", "keyName" => "id" ],
+	],
+
+	// Parameters
+
+	"parameters" => [
+	],
+
+];
+
