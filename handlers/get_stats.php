11de423021e0288831762e8f80ba247a7360efd3
Masaki Yasutake <my17560@gmail.com>
2019-12-22 19:29:53 +0900

    Modified to get latest n months.

--- handlers/get_stats.php
+++ handlers/get_stats.php
@@ -13,7 +13,8 @@ function getSql($title)
 
 	switch ($title) {
 	case "monthly":
-		$sql = "SELECT SUBSTR(payment_dt, 0, 7) YM, category_id CATEGORY, SUM(total_prc) TOTAL FROM `transaction` WHERE from_account_id IS NOT NULL AND to_account_id IS NULL GROUP BY SUBSTR(payment_dt, 0, 7), category_id ORDER BY SUBSTR(payment_dt, 0, 7) DESC, category_id";
+		$target = date("Ym", strtotime("-9 months")) . "01";
+		$sql = "SELECT SUBSTR(payment_dt, 0, 7) YM, category_id CATEGORY, SUM(total_prc) TOTAL FROM `transaction` WHERE from_account_id IS NOT NULL AND to_account_id IS NULL AND payment_dt >= '". $target . "' GROUP BY SUBSTR(payment_dt, 0, 7), category_id ORDER BY SUBSTR(payment_dt, 0, 7) DESC, category_id";
 		break;
 	}
 
